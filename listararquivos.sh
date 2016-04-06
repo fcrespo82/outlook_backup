11518a1066fe8e47932de4db16102ab1b3fb4704
Fernando Crespo <fernando82@gmail.com>
2016-04-06 00:25:41 -0300

    Listar arquivos html por pasta

---
+++ listararquivos.sh
@@ -0,0 +1,3 @@
+
+
+echo "enviadas"; ls outlook_backup_app/static/output/enviadas/*.html | wc -l; echo "entrada"; ls outlook_backup_app/static/output/entrada/*.html | wc -l; echo "arquivo"; ls outlook_backup_app/static/output/arquivo/*.html | wc -l
